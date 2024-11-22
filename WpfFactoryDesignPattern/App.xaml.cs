f6d0e5880af9e67e681f2f4a45beeb8b8961196a
tauhid-islam4 <tauhid.islam4@gmail.com>
2024-11-22 11:22:26 -0500

    Add Factory Design Pattern example

---
+++ WpfFactoryDesignPattern/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfFactoryDesignPattern
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfFactoryDesignPattern/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFactoryDesignPattern
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateCircle_Click(object sender, RoutedEventArgs e)
+        {
+            var shape = ShapeFactory.GetShape("Circle");
+            ShapeInfo.Text = shape.Draw();
+        }
+
+        private void CreateSquare_Click(object sender, RoutedEventArgs e)
+        {
+            var shape = ShapeFactory.GetShape("Square");
+            ShapeInfo.Text = shape.Draw();
+        }
+    }
+
+    public interface IShape
+    {
+        string Draw();
+    }
+
+    public class Circle : IShape
+    {
+        public string Draw() => "Drawing a Circle";
+    }
+
+    public class Square : IShape
+    {
+        public string Draw() => "Drawing a Square";
+    }
+
+    public static class ShapeFactory
+    {
+        public static IShape GetShape(string shapeType)
+        {
+            return shapeType switch
+            {
+                "Circle" => new Circle(),
+                "Square" => new Square(),
+                _ => throw new ArgumentException("Invalid shape type")
+            };
+        }
+    }
+}
